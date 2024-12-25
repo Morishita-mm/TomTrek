8eb85a55e1a785637800f09a55bfa9f56e9dde4e
Morishita-mm <mizuki.no.onaka@gmail.com>
2024-12-25 16:46:14 +0900

    setup project

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    @DisplayName("Test Main")
+    public void testMain() {
+        assertTrue(true);
+    }
+}
