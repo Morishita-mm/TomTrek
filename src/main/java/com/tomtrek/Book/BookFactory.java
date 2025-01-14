3b63004a6b8c90a531b1942f943ddfb0c297a6b4
Morishita-mm <mizuki.no.onaka@gmail.com>
2025-01-14 15:41:44 +0900

    📝 BookFactoryクラスのメソッドに引数の例外処理に関するJavadocコメントを追加

--- src/main/java/com/tomtrek/Book/BookFactory.java
+++ src/main/java/com/tomtrek/Book/BookFactory.java
@@ -1,11 +1,17 @@
 package com.tomtrek.Book;
 
 public class BookFactory {
+    /*
+     * throw IllegalArgumentException isbn形式、日付の形式、未来の日付が渡された時にスローされる
+     */
     public static Book createNewBook(String title, String author, String isbn) {
         return new Book.Builder(title, author, Isbn.create(isbn))
                 .build();
     }
 
+    /*
+     * throw IllegalArgumentException isbn形式、日付の形式、未来の日付が渡された時にスローされる
+     */
     public static Book createReadingBook(String title, String author, String isbn, ReadingDate beginDate) {
         return new Book.Builder(title, author, Isbn.create(isbn))
                 .setBeginReadingDate(beginDate)
@@ -13,7 +19,11 @@ public class BookFactory {
                 .build();
     }
 
-    public static Book createFinishedBook(String title, String author, String isbn, ReadingDate beginDate, ReadingDate finishDate) {
+    /*
+     * throw IllegalArgumentException isbn形式、日付の形式、未来の日付が渡された時にスローされる
+     */
+    public static Book createFinishedBook(String title, String author, String isbn, ReadingDate beginDate,
+            ReadingDate finishDate) {
         return new Book.Builder(title, author, Isbn.create(isbn))
                 .setBeginReadingDate(beginDate)
                 .setFinishReadingDate(finishDate)
